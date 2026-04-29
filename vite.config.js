f28e4d22025a5781424e67489bf2d949d4bf0553
Puneet Kumar <122473392+pun234@users.noreply.github.com>
2026-04-29 15:05:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Ensure this is imported
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Ensure this is in the plugins array
-  ],
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'ai-nutrition-recommendation-1.onrender.com'
+    ],
+   
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0'
+  }
 })
